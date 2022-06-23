aebc54dac251ce7f2e42427d1d58df9c4a059d1f
Tiago <tiago.2000lopes@gmail.com>
2022-06-23 01:39:27 +0100

    initial Author Routes

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ if(process.env.NODE_ENV !== "production")[
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-
+const bodyParser = require("body-parser")
 
 const indexRouter = require("./routes/index");
+const authorsRouter = require("./routes/authors");
 
 app.set('view engine','ejs');
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:"10mb", extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
@@ -24,5 +26,6 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
 
 app.use("/",indexRouter);
+app.use("/authors",authorsRouter);
 
 app.listen(process.env.PORT || 3000);
